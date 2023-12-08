251bba393b5c39aabdc513e3c194388a247f78aa
Alwyn Kik <alwyn@kik.pw>
2023-12-08 23:02:11 +0100

    Improve utilities, sort_policy(), add function for loading and stripping environment variables

--- minio_manager/utilities.py
+++ minio_manager/utilities.py
@@ -4,20 +4,21 @@ import os
 
 import yaml
 
+logger = logging.getLogger("root")
 
-def read_yaml(file):
+
+def read_yaml(file) -> dict:
     with open(file) as f:
         return yaml.safe_load(f)
 
 
-def read_json(file):
+def read_json(file) -> dict:
     with open(file) as f:
         return json.load(f)
 
 
 def setup_logging():
     log_debug = os.environ.get("LOG_LEVEL", "DEBUG")
-    logger = logging.getLogger("root")
     if log_debug:
         logger.setLevel(logging.DEBUG)
         logging.basicConfig(format="[%(filename)s:%(lineno)d	- %(funcName)20s() ] %(message)s")
@@ -42,5 +43,21 @@ def sort_policy(policy: dict):
         statement["Action"] = sorted(statement["Action"])
         statement["Resource"] = sorted(statement["Resource"])
         # TODO: also sort Principals
+        statement = dict(sorted(statement.items()))
         policy["Statement"][index] = statement
     return policy
+
+
+def retrieve_environment_variable(name: str) -> str:
+    """
+    Get an environment variable and strip any leading and trailing single and double quotes.
+    This is because Python apparently literally loads them.
+    Args:
+        name: str, the name of the environment variable
+
+    Returns: str stripped
+    """
+    variable = os.environ[name]
+    strip_double_quotes = variable.strip('"')
+    strip_single_quotes = strip_double_quotes.strip("'")
+    return strip_single_quotes
