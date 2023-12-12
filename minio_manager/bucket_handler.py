8e173ae138455899fd1c2f8e049bf20a59a1bcdd
Alwyn Kik <alwyn@kik.pw>
2023-12-12 23:45:56 +0100

    Check if bucket versioning is enabled

--- minio_manager/bucket_handler.py
+++ minio_manager/bucket_handler.py
@@ -13,7 +13,11 @@ def handle_bucket(client: Minio, bucket):
     else:
         logger.info(f"Bucket {bucket['name']} already exists")
 
-    if bucket["versioning"]:
-        client.set_bucket_versioning(bucket["name"], VersioningConfig(ENABLED))
-        versioning_status = client.get_bucket_versioning(bucket["name"]).status
-        logger.debug(f"Versioning status for bucket {bucket['name']}: {versioning_status}")
+    if not bucket["versioning"]:
+        return
+
+    versioning_status = client.get_bucket_versioning(bucket["name"]).status
+    if versioning_status == ENABLED:
+        return
+    client.set_bucket_versioning(bucket["name"], VersioningConfig(ENABLED))
+    logger.debug(f"Versioning enabled for bucket {bucket['name']}")
