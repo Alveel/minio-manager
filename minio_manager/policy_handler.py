d9048cf6e1bcb464cf68e8dcc1fdc977f6b93022
Alwyn Kik <alwyn@kik.pw>
2023-12-12 23:47:19 +0100

    Update policy_handler

--- minio_manager/policy_handler.py
+++ minio_manager/policy_handler.py
@@ -35,8 +35,8 @@ def handle_bucket_policy(client: Minio, policy):
             logger.info(f"Creating bucket policy for {policy['bucket']}")
             client.set_bucket_policy(policy["bucket"], desired_policy_json)
             current_policy = client.get_bucket_policy(policy["bucket"])
-    else:
-        logger.exception("An unknown exception occurred")
+    except Exception:
+        raise
 
     if current_policy == desired_policy:
         return
@@ -66,6 +66,7 @@ def handle_iam_policy(client: MinioAdmin, iam_policy):
         logger.debug(f"Current (sorted) policy: {current_policy}")
         logger.debug(f"Desired (sorted) policy: {desired_policy}")
     except MinioAdminException as mae:
+        # noinspection PyProtectedMember
         mae_obj = json.loads(mae._body)
         if mae_obj["Code"] == "XMinioAdminNoSuchPolicy":
             logger.info(f"IAM policy {iam_policy['name']} does not exist, creating.")
@@ -86,12 +87,14 @@ def handle_user_policy_attachments(client: MinioAdmin, user):
     Manage user policy attachments.
 
     Args:
-        user: str
+        user: dict
+            user: str
+            policies: list
         client: MinioAdmin
     """
-    logger.debug(f"Handling user policy attachments for '{user['user']}'")
+    logger.debug(f"Handling user policy attachments for '{user['name']}'")
     for policy_name in user["policies"]:
-        logger.debug(f"Attaching policy '{policy_name}' to user '{user['user']}'")
-        client.policy_set(policy_name, user["user"])
+        logger.debug(f"Attaching policy '{policy_name}' to access key '{user['access_key']}'")
+        client.policy_set(policy_name, user["access_key"])
 
     # TODO: don't set the attachments if they're already attached
