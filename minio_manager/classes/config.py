42ede999045e7f54c8fecf1e3c10e3b50c496123
Alwyn Kik <alwyn@kik.pw>
2024-01-16 18:26:39 +0100

    Loop over resources twice to first validate them all before handling them

--- minio_manager/classes/config.py
+++ minio_manager/classes/config.py
@@ -1,5 +1,7 @@
 import yaml
 
+from .minio_resources import Bucket, BucketPolicy, IamPolicy, IamPolicyAttachment, ServiceAccount
+
 
 class ClusterConfig(yaml.YAMLObject):
     """MinIO Cluster configuration object, aka the cluster contents: buckets, policies, etc."""
@@ -20,3 +22,24 @@ class ClusterConfig(yaml.YAMLObject):
         self.service_accounts = service_accounts
         self.iam_policies = iam_policies
         self.iam_policy_attachments = iam_policy_attachments
+
+
+def parse_resources(cluster_config: ClusterConfig) -> tuple:
+    service_accounts, buckets, bucket_policies, iam_policies, iam_policy_attachments = [], [], [], [], []
+
+    for service_account in cluster_config.service_accounts:
+        service_accounts.append(ServiceAccount(service_account["name"]))
+
+    for bucket in cluster_config.buckets:
+        buckets.append(Bucket(bucket["name"], bucket["versioning"]))
+
+    for bucket_policy in cluster_config.bucket_policies:
+        bucket_policies.append(BucketPolicy(bucket_policy["bucket"], bucket_policy["policy_file"]))
+
+    for iam_policy in cluster_config.iam_policies:
+        iam_policies.append(IamPolicy(iam_policy["name"], iam_policy["policy_file"]))
+
+    for user in cluster_config.iam_policy_attachments:
+        iam_policy_attachments.append(IamPolicyAttachment(user["name"], user["policies"]))
+
+    return service_accounts, buckets, bucket_policies, iam_policies, iam_policy_attachments
