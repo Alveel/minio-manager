2f698217bf08cf4af90d1517a73e51051649cc80
Alwyn Kik <alwyn@kik.pw>
2024-02-02 21:24:13 +0100

    Load .env file from current working directory

--- minio_manager/app.py
+++ minio_manager/app.py
@@ -1,6 +1,6 @@
 import time
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 
 from minio_manager.classes.config import ClusterResources
 from minio_manager.clients import get_minio_config, get_secret_manager
@@ -9,7 +9,8 @@ from minio_manager.utilities import logger
 
 
 def main():
-    load_dotenv()
+    # Load environment variables from .env file from the current working directory.
+    load_dotenv(find_dotenv(usecwd=True), verbose=True)
     logger.info("Starting MinIO Manager...")
     start_time = time.time()
     config = get_minio_config()
