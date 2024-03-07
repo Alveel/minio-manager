4aad1c34652d765b03e98447929113027a1818d3
Alwyn Kik <alwyn@kik.pw>
2024-03-07 18:06:23 +0100

    Update example pipeline.sh to reflect example changes

--- examples/pipeline.sh
+++ examples/pipeline.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 set -eu
 
-for cluster in */; do
-  cluster=$(basename $cluster)
-
-  for group in $cluster/*/; do
-    (
-      group=$(basename $group)
-      if [ -s "$cluster/$group/.env" ]; then
-        cd "$cluster/$group"
-        source .env
-        echo "Running minio-manager for $cluster/$group"
-        minio-manager
-      else
-        echo "Skipping $cluster/$group because .env is missing"
-      fi
-    )
-  done
+for group in */; do
+  (
+    group=$(basename $group)
+    if [ -s "$cluster/$group/.env" ]; then
+      cd "$cluster/$group"
+      source .env
+      echo "Running minio-manager for $cluster/$group"
+      minio-manager
+    else
+      echo "Skipping $cluster/$group because .env is missing"
+    fi
+  )
 done
