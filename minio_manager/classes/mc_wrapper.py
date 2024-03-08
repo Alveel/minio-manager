355a00c882c6cd3e133e375de7e8d06fc33e8182
Alwyn Kik <alwyn@kik.pw>
2024-03-08 14:12:03 +0100

    Fix regression

--- minio_manager/classes/mc_wrapper.py
+++ minio_manager/classes/mc_wrapper.py
@@ -102,7 +102,7 @@ class McWrapper:
         multiline = cmd in ["list", "ls"]
         resp = self._run(["admin", "user", "svcacct", cmd, self.cluster_name, *args], multiline=multiline)
         resp_error = resp[0] if multiline else resp
-        if hasattr(resp_error, "error"):
+        if "error" in resp_error:
             resp_error = resp_error["error"]
             error_details = resp_error["cause"]["error"]
             raise_specific_error(error_details["Code"], error_details["Message"])
