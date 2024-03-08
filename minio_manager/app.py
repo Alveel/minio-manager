2c97a5e04611b97ed255161ca6687c736f1615c5
Alwyn Kik <alwyn@kik.pw>
2024-03-08 18:07:45 +0100

    Improve logging for debug purposes

--- minio_manager/app.py
+++ minio_manager/app.py
@@ -1,18 +1,29 @@
+import sys
 import time
+from logging import DEBUG
 
 from dotenv import find_dotenv, load_dotenv
 
 from minio_manager.classes.config import ClusterResources
 from minio_manager.clients import get_minio_config, get_secret_manager
 from minio_manager.resource_handler import handle_resources
-from minio_manager.utilities import logger
+from minio_manager.utilities import init_debug, logger
 
 
 def main():
-    # Load environment variables from .env file from the current working directory.
-    load_dotenv(find_dotenv(filename="config.env", usecwd=True), override=True, verbose=True)
-    logger.info("Starting MinIO Manager...")
     start_time = time.time()
+    logger.info("Starting MinIO Manager...")
+
+    # Load environment variables from .env file from the current working directory.
+    logger.debug("Loading config.env file from current working directory...")
+    de_loaded = load_dotenv(find_dotenv(filename="config.env", usecwd=True), override=True, verbose=True)
+
+    if not de_loaded:
+        logger.debug("Failed to load config.env file from current working directory!")
+        sys.exit(1)
+    if logger.level == DEBUG:
+        init_debug()
+
     config = get_minio_config()
 
     logger.info("Loading and parsing resources...")
