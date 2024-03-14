f67ab6347e86bc77d5e52b26743ac2cf33178c63
Alwyn Kik <alwyn@kik.pw>
2024-03-14 18:14:33 +0100

    Don't sort imports in __init__

--- minio_manager/__init__.py
+++ minio_manager/__init__.py
@@ -1,3 +1,4 @@
-# Initialise the settings
-from minio_manager.classes.logging_config import logger as logger
+# Initialise settings and logging
+# ruff: noqa: I001
 from minio_manager.classes.settings import settings as settings
+from minio_manager.classes.logging_config import logger as logger
