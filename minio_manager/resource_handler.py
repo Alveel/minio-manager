96bd3ab644112d336a5058292e167ff266736842
Alwyn Kik <alwyn@kik.pw>
2025-04-30 13:39:45 +0200

    fix: only handle resources that are configured

--- minio_manager/resource_handler.py
+++ minio_manager/resource_handler.py
@@ -15,18 +15,22 @@ def handle_resources(resources: ClusterResources):
     for bucket in resources.buckets:
         handle_bucket(bucket)
 
-    logger.info(f"Handling {len(resources.bucket_policies)} bucket policies...")
-    for bucket_policy in resources.bucket_policies:
-        handle_bucket_policy(bucket_policy)
+    if resources.bucket_policies:
+        logger.info(f"Handling {len(resources.bucket_policies)} bucket policies...")
+        for bucket_policy in resources.bucket_policies:
+            handle_bucket_policy(bucket_policy)
 
-    logger.info(f"Handling {len(resources.service_accounts)} service accounts...")
-    for service_account in resources.service_accounts:
-        handle_service_account(service_account)
+    if resources.service_accounts:
+        logger.info(f"Handling {len(resources.service_accounts)} service accounts...")
+        for service_account in resources.service_accounts:
+            handle_service_account(service_account)
 
-    logger.info(f"Handling {len(resources.iam_policies)} IAM policies...")
-    for iam_policy in resources.iam_policies:
-        handle_iam_policy(iam_policy)
+    if resources.iam_policies:
+        logger.info(f"Handling {len(resources.iam_policies)} IAM policies...")
+        for iam_policy in resources.iam_policies:
+            handle_iam_policy(iam_policy)
 
-    logger.info(f"Handling {len(resources.iam_policy_attachments)} IAM policy attachments...")
-    for iam_policy_attachment in resources.iam_policy_attachments:
-        handle_iam_policy_attachments(iam_policy_attachment)
+    if resources.iam_policy_attachments:
+        logger.info(f"Handling {len(resources.iam_policy_attachments)} IAM policy attachments...")
+        for iam_policy_attachment in resources.iam_policy_attachments:
+            handle_iam_policy_attachments(iam_policy_attachment)
